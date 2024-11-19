3048b653c21b2d0e80d352bc57732cc68710c5e3
Diego Vasconcelos <diegovasconcelosb@gmail.com>
2024-11-18 21:59:54 -0300

    feat: criando UserService

--- BibliotecaDefinitiva.Application/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace BibliotecaDefinitiva.Application;
-
-public class Class1
-{
-
-}
---
+++ BibliotecaDefinitiva.Application/UserService.cs
@@ -0,0 +1,18 @@
+﻿using BibliotecaDefinitiva.Domain.Entities;
+using BibliotecaDefinitiva.Domain.Interfaces;
+
+namespace BibliotecaDefinitiva.Application.Services
+{
+    public class UserService
+    {
+        private readonly IUserRepository _repository;
+
+        public UserService(IUserRepository repository) => _repository = repository;
+
+        public async Task<IEnumerable<User>> GetAllUsers() => await _repository.GetAllUsers();
+        public async Task<IEnumerable<User>> GetUserById(int id) => (IEnumerable<User>)await _repository.GetUserById(id);
+        public async Task<User> AddUser(User user) => await _repository.AddUser(user);
+        public async Task UpdateUser(User user) => await _repository.UpdateUser(user);
+        public async Task DeleteUser(int id) => await _repository.DeleteUser(id);
+    }
+}
